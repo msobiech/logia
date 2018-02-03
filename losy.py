76f7dc691c66af3ec83adbb11b8acb8229772645
msobiech <maks.sobiech@gmail.com>
2018-02-03 17:07:22 +0100

    logia 13

---
+++ losy.py
@@ -0,0 +1,36 @@
+from turtle import *
+
+p = Turtle()
+
+def platek():
+    p.begin_fill()
+    p.fillcolor("pink")
+    for i in range(2):
+        p.fd(60)
+        p.rt(360/8)
+        p.fd(60)
+        p.rt(180-(360/8))
+    p.end_fill()
+
+def czysc():
+    p.begin_fill()
+    p.color("white")
+    for i in range(2):
+        p.fd(60)
+        p.rt(360/8)
+        p.fd(60)
+        p.rt(180-(360/8))
+    p.end_fill()
+        
+def kwiatek(start,ile):
+    for i in range(8):
+        platek()
+        p.rt(360/8)
+    p.lt(90)
+    p.rt(360/8*(start-1))
+    for i in range(ile):
+        czysc()
+        p.rt(360/8)
+    
+        
+kwiatek(3,4)
